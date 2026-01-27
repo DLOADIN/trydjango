192c8932f480baf5cc288b7a620b8908caeec3ba
DLOADIN <m.david@alustudent.com>
2026-01-27 11:44:34 +0200

    dhh

--- newlearning/urls.py
+++ newlearning/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from products import views                                                   
 
 urlpatterns = [
+    path('products/', views.product_detail_view),
     path('admin/', admin.site.urls),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def product_detail_view(*args, **kwargs):
+    return HttpResponse("<h1>Product Detail View</h1>")
